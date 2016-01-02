89e36a62735a3225aeb825f49a2f04a38c4f21f5
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2016-01-02 08:21:52 -0500

    yoda-uncertainty.py: cleanup, remove scale uncertainty that now is superseded by yoda-envelope.py

--- yoda-uncertainty.py
+++ yoda-uncertainty.py
@@ -3,9 +3,6 @@
 import yoda
 import numpy as np
 
-def calc_deviation(xs):
-    return np.sqrt(np.mean(np.array(xs)**2))
-
 def master_formula(get_value_func, num_members):
     val_0 = get_value_func(0)
     dys_p_sq = np.zeros(len(val_0.xs))
@@ -20,14 +17,6 @@ def master_formula(get_value_func, num_members):
     val_0.dys_p = np.sqrt(dys_p_sq)
     val_0.dys_n = np.sqrt(dys_n_sq)
     return val_0
-
-def scale_uncertainty(central_xsec, xsecs):
-    central_xsec.dys_p = np.zeros(len(central_xsec.ys))
-    central_xsec.dys_n = np.zeros(len(central_xsec.ys))
-    for xsec in xsecs:
-        central_xsec.dys_p = np.max([central_xsec.dys_p, xsec.ys - central_xsec.ys], 0)
-        central_xsec.dys_n = np.max([central_xsec.dys_n, central_xsec.ys - xsec.ys], 0)
-    return central_xsec
 
 class XSec(object):
     pass
@@ -53,19 +42,3 @@ for (low, center, high, y, dp, dn) in zip(xsec.xs_low, xsec.xs, xsec.xs_high, xs
     s.addPoint(center, y, xerrs=[center-low, high-center], yerrs=[dn, dp])
 
 yoda.writeYODA([s], "pdf.yoda")
-
-xsecs = [
-    read_xsec("../CT10nlo_0.5_1.0.yoda"),
-    read_xsec("../CT10nlo_2.0_1.0.yoda"),
-    read_xsec("../CT10nlo_1.0_0.5.yoda"),
-    read_xsec("../CT10nlo_1.0_2.0.yoda"),
-    read_xsec("../CT10nlo_0.5_0.5.yoda"),
-    read_xsec("../CT10nlo_2.0_2.0.yoda"),
-    ]
-xsec = scale_uncertainty(read_xsec("../CT10nlo_00.yoda"), xsecs)
-
-s = yoda.Scatter2D("/STAR_DIJET/d01-x01-y01")
-for (low, center, high, y, dp, dn) in zip(xsec.xs_low, xsec.xs, xsec.xs_high, xsec.ys, xsec.dys_p, xsec.dys_n):
-    s.addPoint(center, y, xerrs=[center-low, high-center], yerrs=[dn, dp])
-
-yoda.writeYODA([s], "scale.yoda")
