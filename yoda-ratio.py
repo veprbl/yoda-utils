d1343b2c8649c6cb5b1346ed6bcb1e91704ccc84
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2016-01-12 13:48:47 -0500

    yoda-ratio: support dividing Scatter2D objects

--- yoda-ratio.py
+++ yoda-ratio.py
@@ -5,6 +5,21 @@ import sys
 import yoda
 
 
+def find_point(scatter, x):
+    for point in scatter:
+        if point.x == x:
+            return point
+    raise IndexError("Matching point not found")
+
+def div2d(s1, s2):
+    s = yoda.Scatter2D()
+    for p1 in s1:
+        p2 = find_point(s2, p1.x)
+        if p2.y != 0:
+            s.addPoint(p1.x, p1.y / p2.y, xerrs = p1.xErrs)
+        else:
+            s.addPoint(p1.x, 0, xerrs = p1.xErrs)
+    return s
 
 if __name__ == '__main__':
     if (len(sys.argv) != 4):
@@ -20,10 +35,14 @@ if __name__ == '__main__':
 
     out_aos = []
     for ao in aos1.values():
-        if not isinstance(ao, yoda.core.Histo1D):
-            continue
         ao2 = aos2[ao.path]
-        r = ao / ao2
+        if isinstance(ao, yoda.core.Histo1D):
+            assert isinstance(ao2, yoda.core.Histo1D)
+            r = ao / ao2
+        elif isinstance(ao, yoda.core.Scatter2D):
+            r = div2d(ao, ao2)
+        else:
+            continue
         r.path = ao.path
         r.setAnnotation("LogY", 0)
         out_aos.append(r)
